d12dda916feb14db233bd265d260e4422a12895f
li <li@goldragon.criome.net>
2026-06-19 13:00:39 +0200

    signal-mirror: bump to 0.1.1 for schema chain

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use schema_rust_next::build::ContractCrateBuild;
 fn main() {
     ContractCrateBuild::from_environment(
         "signal-mirror",
-        "0.1.0",
+        "0.1.1",
         "SIGNAL_MIRROR_UPDATE_SCHEMA_ARTIFACTS",
     )
     .expect_fresh();
